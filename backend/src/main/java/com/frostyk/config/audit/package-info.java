9919c7f333918a9abe2d54f5fa55ccf5c76d9679
BVDeveloper3 bvdeveloper3@outlook.com <bvdeveloper3@outlook.com>
2019-03-11 11:23:00 +0200

    Initial application generated by JHipster-5.8.2

---
+++ backend/src/main/java/com/frostyk/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.frostyk.config.audit;
---
+++ backend/src/main/java/com/frostyk/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.frostyk.repository;
+
+import com.frostyk.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
